3c83932cc7c00e97a4cb7ff0da17b060af3fe641
JoseCutileiro <72456290+JoseCutileiro@users.noreply.github.com>
2021-12-29 21:32:29 +0000

    BUP 2

---
+++ Cengine2.0/BUP2/Sprites.h
@@ -0,0 +1,26 @@
+#ifndef _SPRITES_
+#define _SPRITES_
+
+
+/* POINT: *
+ *  ###   *
+ * #####  *
+ *  ###   */
+
+#define POINT ",,,###,,,,,#####,,,,,###,,,"
+#define POINT_W 9
+#define POINT_H 3
+
+#define LEONOR "###    ###### ###### ##    # ###### ########    #      #    # # #   # #    # #  # ###    ###    #    # #  #  # #    # ###  #####  #      #    # #   # # #    # # ## #####  #####  ###### #    ## ###### #   #"
+#define LEONOR_W 41
+#define LEONOR_H 5
+
+#define TREE ",,,,####,,,,,,,######,,,,,,######,,,,,,,####,,,,,,,,,||,,,,,,,,,,||,,,,,,,,,,||,,,,,"
+#define TREE_W 12
+#define TREE_H 7
+
+#define SQUARE "______|,,,,||,,,,||____|"
+#define SQUARE_W 6
+#define SQUARE_H 4
+
+#endif
